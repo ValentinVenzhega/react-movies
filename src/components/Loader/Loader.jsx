c5c6f2755dc6ecb893edba844bc7b026a3cb6f90
Valentin Venzhega <valek917@yandex.ru>
2023-02-02 19:22:50 +0300

    add loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./Loader.module.scss";
+
+export const Loader = () => {
+    return (
+        <div>
+            <svg className={styles.spinner} viewBox="0 0 50 50">
+                <circle
+                    className={styles.path}
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    fill="none"
+                    strokeWidth="5"
+                ></circle>
+            </svg>
+        </div>
+    );
+};
